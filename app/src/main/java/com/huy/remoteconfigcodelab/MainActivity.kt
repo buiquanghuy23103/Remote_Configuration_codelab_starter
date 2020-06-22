40ea3619b80fee92b0a6f1bee46475e2e68396ca
Huy Bui <abcd@email.com>
2020-06-22 11:50:48 +0300

    Setup starter code

--- app/src/main/java/com/huy/remoteconfigcodelab/MainActivity.kt
+++ app/src/main/java/com/huy/remoteconfigcodelab/MainActivity.kt
@@ -2,10 +2,44 @@ package com.huy.remoteconfigcodelab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        initRemoteConfig()
+    }
+
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.refresh_config -> {
+                fetchConfig()
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun initRemoteConfig() {
+
+        // TODO: set up Remote Config service
+
+        fetchConfig()
+    }
+
+    private fun fetchConfig() {
+        // TODO: fetch and apply Remote Config parameters
+
     }
 }
